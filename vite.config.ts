c28746deb00257fe21feaaae1a6d5b936d05a9b5
Gentle_Lijie <zljzljsweepy@qq.com>
2025-11-06 14:08:48 +0800

    fix: employ different base url for different environment

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/debate-timer/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/debate-timer/' : '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+}));
